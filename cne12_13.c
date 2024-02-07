8e3d085d4be3ec29fd5e5f6e6ae99c767d275070
Yuva <114496360+Yuvalakshmii@users.noreply.github.com>
2024-02-07 09:24:45 +0530

    Update cne12_13.c

--- cne12_13.c
+++ cne12_13.c
@@ -1,1 +1,13 @@
 https://asecuritysite.com/comms/crc_div
+/*write a c program of Cyclic redundancy check (CRC) to print the codeword on server side.
+1. On client side, it should get input of data and divisor, which should be transmitted to serever to perform CRC calculation on server side.
+2. on server side, it should output the codeword(data+CRC), whee CRC is the last three digits of the codeword.
+input:
+enter data: 1001
+enter divisor: 1011
+
+output:
+codeword: 1001110
+
+the CRC in the output is last three digit: 110
+*/
